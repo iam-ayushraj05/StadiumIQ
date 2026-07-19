d4e6b93d28affdf68a8c2b9f075b5ab2f6c50930
iam-ayushraj05 <offayushraj1006@gmail.com>
2026-07-19 20:05:38 +0530

    Configure project for Vercel deployment

--- js/api.js
+++ js/api.js
@@ -3,7 +3,9 @@
  * Frontend service layer to communicate with the Express backend
  */
 
-const API_BASE = 'http://localhost:3001/api';
+const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://localhost:3001/api'
+  : '/api';
 
 class ApiClient {
   constructor() {
